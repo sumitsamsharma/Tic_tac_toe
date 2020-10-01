231fc216676ec784f19cbae2f1351b5d3519d10b
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-01 11:16:35 +0530

    Refactored choose option

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,40 +2,35 @@ package com.cg.tictactoe;
 
 import java.util.Scanner;
 
-public class TicTacToeGame 
-{
+public class TicTacToeGame {
 	static Scanner scanner = new Scanner(System.in);
-	public static final int Size=10;
-	
+	public static final int Size = 10;
+
 	/**
 	 * Initialised new board
 	 */
-	public static void CreateBoard() 
-	{
-	   char[] board = new char[Size];
-	   for(int index=0;index<Size;index++)
-	   {
-		   board[index]=' ';
-	   }
-	   System.out.println("Board initialised");
+	public static void createBoard() {
+		char[] board = new char[Size];
+		for (int index = 0; index < Size; index++) {
+			board[index] = ' ';
+		}
+		System.out.println("Board initialised");
 	}
-	
+
 	/**
 	 * Choosing option
 	 */
-	public static char PlayerInputs()
-	{
+	public static char playerInputs() {
 		System.out.println("Choose X or O");
-		char chosen = scanner.next().charAt(0); 
-		return Character.toUpperCase(chosen);
+		return Character.toUpperCase(scanner.next().charAt(0));
 	}
-	
-	public static void main(String[] args)
+
+	public static void main(String[] args) 
 	{
-	   System.out.println("Welcome to Tic Tac Toe game ");
-	   CreateBoard();
-	   char PlayerOption=PlayerInputs();
-	   char ComputerOption= (PlayerOption=='X')?'O':'X';
-	   System.out.println("Player chose: "+PlayerOption+" Computer chose: "+ComputerOption);
+		System.out.println("Welcome to Tic Tac Toe game ");
+		createBoard();
+		char player = playerInputs();
+		char computer = (player == 'X') ? 'O' : 'X';
+		System.out.println("Player chose: " + player + " Computer chose: " + computer);
 	}
 }
