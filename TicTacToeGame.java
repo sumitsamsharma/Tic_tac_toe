bcdd9f4b1a4c8537cefe9f245992d735952539d1
sumitsamsharma <sharmasumit94286@gmail.com>
2020-10-01 10:23:41 +0530

    Added choose option

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.cg.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame 
 {
 	public static final int Size=10;
@@ -16,9 +18,20 @@ public class TicTacToeGame
 	   System.out.println("Board initialised");
 	}
 	
+	public static char PlayerInputs()
+	{
+		System.out.println("Choose X or O");
+		Scanner sc = new Scanner(System.in);
+		char chosen = sc.next().charAt(0); 
+		return chosen;
+	}
+	
 	public static void main(String[] args)
 	{
 	   System.out.println("Welcome to Tic Tac Toe game ");
-	   CreateBoard();	     
+	   CreateBoard();
+	   char option=PlayerInputs();
+	   System.out.println("Player 1 choose: "+option);
+	   System.out.println("Computer choose: O");   
 	}
 }
